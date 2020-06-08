e8c6bbc7b5f1880f7a0c60a1f0e5435cfaa31a25
Ali Shaikh <31328423+AliNAU2020@users.noreply.github.com>
2020-06-08 01:55:26 -0500

    Add files via upload

---
+++ Chinese Zodiac/Includes/inc_home_links_bar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home Link Bar </title>
+</head>
+<style type ="text/css">
+	ul{
+		background-color: #debe0b;
+		margin: auto;
+		padding:8px;
+	  }
+</style>
+<body>
+	<div>
+		<ul style = "list-style: none;">
+			<li><a href = "index.php?page=home_page&section=php">PHP</a></li>
+			<li><a href = "index.php?page=home_page&section=zodiac">Chinese Zodiac</a></li>
+			<li><a href = "index.php?page=home_page&section=feedback">Feedback</a></li>
+			
+		</ul>
+	</div>
+</body>
+</html>
+			
